15d68cbbd586cc2e5029f7ae6a44a63a8efbff62
heqqqqx <92042040+heqqqqx@users.noreply.github.com>
2021-12-10 09:43:04 +0100

    fix the roles in the company from Amaury DAVOINE

--- my_function.py
+++ my_function.py
@@ -2,11 +2,12 @@
 
 def Company_list(Company):
 
-    print("The CEO is : \n", Company['CEO'],
+    print("The Scrum Master is : \n", Company['CEO'],
           "\nThe Project Manager is : \n", Company['Project Manager'],
           "\nThe first programmer is :\n", Company['Programmer 1'],
           "\nThe second one is :\n", Company['Programmer 2'],
           "\nThe third one is :\n", Company['Programmer 3'],
+          "\nThe fourth one is :\n", Company['Programmer 3'],
           "\nThe last programmer is :\n", Company['Programmer 5'] )
 
 
@@ -15,8 +16,9 @@ Company = {
     'Programmer 1': 'Tristan',
     'Programmer 2': 'Amaury',
     'Programmer 3': 'Clement',
+    'Programmer 4': 'Kilian',
     'Programmer 5': 'Tshiya',
-    'CEO': 'Jean',
+    'Scrum Master': 'Jean',
     'Project Manager': 'Marc-Antoine'
     
 }
